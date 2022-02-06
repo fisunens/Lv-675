c099a8711de45a936a6649fb3e0f5ced71fa4ca7
Osaulenko Oleksii <fisunens@gmail.com>
2022-02-07 01:32:58 +0200

    Create directory and rebase my homework. First part HW2 is done

---
+++ Osaulenko Oleksii/Lecture_2/HW2_1.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Lecture2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float min_num, max_num, f_num;
+            string message;
+            min_num = -5;
+            max_num = 5;
+            foreach (int i in Enumerable.Range(1, 3))
+            {
+                Console.WriteLine($"Please enter float nomber №{i}");
+                f_num = (float) Convert.ToDouble(Console.ReadLine());
+                _ = (f_num >= min_num && f_num <= max_num) ? message = $"Your number {f_num} in range [-5.5]" : message = $"Your number {f_num} NOT in range [-5.5]";
+                Console.WriteLine(message);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
