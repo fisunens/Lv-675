9d7210358e82c135e7aebb4a2efd5dc09345a52f
Osaulenko Oleksii <fisunens@gmail.com>
2022-02-14 03:53:13 +0200

    Homework 4 is done

---
+++ Osaulenko Oleksii/Lecture_4/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    public class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int birthYear;
+        public int BirthYear
+        {
+            get { return birthYear; }
+            set { birthYear = value; }
+        }
+
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            int age = (DateTime.Now.Year - birthYear);
+            return age;
+        }
+
+        public static Person Input(int i)
+        {
+            Console.Write($"Enter {i + 1} name: ");
+            string name = Console.ReadLine();
+            Console.Write($"Enter {i + 1} birthay year: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+            Person person = new Person(name, birthYear);
+            return person;
+        }
+        public void ChangeName(int age)
+        {
+            if (age < 16)
+            {
+                name = "Very Young";
+            }
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Name - {name}, birthday year - {birthYear}");
+        }
+        public static bool operator == (Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+
+        public static bool operator != (Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+}
---
+++ Osaulenko Oleksii/Lecture_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num_obj = 6;                                // Create 6 object
+            var persons = new Person[num_obj];
+            for (int i = 0; i < persons.Length ; i++)
+            {
+                persons[i] = Person.Input(i);
+            }
+
+            for (int i = 0; i < persons.Length; i++)        // Output name and age of person
+            {
+                int age = persons[i].Age();
+                Console.WriteLine($"Person {i}, name {persons[i].Name}, age {age}");
+            }
+
+            for (int i = 0; i < persons.Length; i++)        // Change name of persons 
+            {
+                int age = persons[i].Age();
+                persons[i].ChangeName(age);
+            }
+          
+            for (int i = 0; i < persons.Length; i++)       // Output info about all
+            {
+                persons[i].Output();
+            }
+            
+            for (int i = 0; i < persons.Length; i++)       // Output info ==
+            {
+                for (int j = i + 1; j < persons.Length; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.WriteLine($"{persons[i].Name} and {persons[j].Name} are equals");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
